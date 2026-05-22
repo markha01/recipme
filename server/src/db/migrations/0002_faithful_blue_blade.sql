0951e786e03744565bbad4233e2b71f3b8f83cad
Maryam Khan <markha@kth.se>
2026-05-22 16:08:59 +0200

    Fix for OSC upload

--- server/src/db/migrations/0002_faithful_blue_blade.sql
+++ server/src/db/migrations/0002_faithful_blue_blade.sql
@@ -4,7 +4,11 @@ CREATE TABLE IF NOT EXISTS "recipe_boards" (
 	CONSTRAINT "recipe_boards_recipe_id_board_id_pk" PRIMARY KEY("recipe_id","board_id")
 );
 --> statement-breakpoint
-ALTER TABLE "recipes" DROP CONSTRAINT "recipes_board_id_boards_id_fk";
+DO $$ BEGIN
+ ALTER TABLE "recipes" DROP CONSTRAINT "recipes_board_id_boards_id_fk";
+EXCEPTION
+ WHEN undefined_object THEN null;
+END $$;
 --> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "recipe_boards" ADD CONSTRAINT "recipe_boards_recipe_id_recipes_id_fk" FOREIGN KEY ("recipe_id") REFERENCES "public"."recipes"("id") ON DELETE cascade ON UPDATE no action;
